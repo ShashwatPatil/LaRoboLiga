0ee26e57048d1dd2c11e726298065963b5b990c8
SGP <shashwatpatil974@gmail.com>
2024-01-03 00:13:31 +0530

    commit

--- PS2/PS2.py
+++ PS2/PS2.py
@@ -5,9 +5,22 @@ import LaRoboLiga24
 import cv2
 
 
-CAR_LOCATION = [2,3,1.5]
-BALL_LOCATION = [4,5,1.5]
-HUMANOID_LOCATION = [-3,-4,1.3]
+CAR_LOCATION = [-5,0,1.5]
+
+BALLS_LOCATION = dict({
+    'red'    : [3,4,1.5],
+    'blue' : [4,-3,1.5],
+    'yellow'   : [2,1,1.5],
+    'green' : [3,-2,1.5]
+})
+
+HUMANOIDS_LOCATION = dict({
+    'red'    : [11,1.5,0.8],
+    'blue' : [-11,-1.5,0.8],
+    'yellow'   : [-1.5,11,8],
+    'green' : [-1.5,-11,8]
+})
+
 VISUAL_CAM_SETTINGS = dict({
     'cam_dist'       : 13,
     'cam_yaw'        : 0,
@@ -26,8 +39,8 @@ os.chdir(os.path.dirname(os.getcwd()))
 env = gym.make('LaRoboLiga24',
     arena = "arena2",
     car_location=CAR_LOCATION,
-    ball_location=BALL_LOCATION,
-    humanoid_location=HUMANOID_LOCATION,
+    ball_location=BALLS_LOCATION,
+    humanoid_location=HUMANOIDS_LOCATION,
     visual_cam_settings=VISUAL_CAM_SETTINGS
 )
 
